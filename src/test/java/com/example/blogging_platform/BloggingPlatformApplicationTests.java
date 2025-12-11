3de7e55687c7178ffb73a1c412ce177e21870f81
Shlok Motwani <shlok.codes@gmail.com>
2025-12-11 09:49:18 +0530

    initialize project

---
+++ src/test/java/com/example/blogging_platform/BloggingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogging_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
