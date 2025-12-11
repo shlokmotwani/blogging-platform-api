10a61a6f17e2290db9f93f07c321240243a32253
Shlok Motwani <shlok.codes@gmail.com>
2025-12-11 10:21:42 +0530

    sample controller to test server

---
+++ src/main/java/com/example/blogging_platform/controllers/SampleController.java
@@ -0,0 +1,14 @@
+package com.example.blogging_platform.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class SampleController {
+    @GetMapping
+    public String hello(){
+        return "Hello, everyone!";
+    }
+}
